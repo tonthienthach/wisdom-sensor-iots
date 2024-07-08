1a2425d5e8fadeb400718a15759ce94320b0d8c5
Thinh <110151184+doanvanthinh20DTH5@users.noreply.github.com>
2024-07-08 20:43:28 +0700

    merge-code
    merge-code

--- src/components/user/Header/Header.jsx
+++ src/components/user/Header/Header.jsx
@@ -1,37 +1,41 @@
+import styles from './header.module.css';
+import logo from '../../../assets/logo.png'; // Import logo image
+
 function Header() {
     return (
         <div>
-            <header>
-                <div className='header-container'>
-                    <img src='../../../images/logo.png' alt='Logo' className='logo1' />
-                    <nav className='nav-menu'>
-                        <a href='../../../html/user/my_account/profile.html' className='nav-link'>
+            <header className={styles.header}>
+                <div className={styles.headerContainer}>
+                    <img src={logo} alt='Logo' className={styles.logo1} />
+                    <nav className={styles.navMenu}>
+                        <a href='../../../html/user/my_account/profile.html' className={styles.navLink}>
                             Home
                         </a>
-                        <a href='../../../html/user/service/servicelist.html' className='nav-link'>
+                        <a href='../../../html/user/service/servicelist.html' className={styles.navLink}>
                             Service Pack
                         </a>
                         <a
                             href='../../../static/html/admin/contract/contract_list.html'
-                            className='nav-link'
+                            className={styles.navLink}
                         >
                             Contact
                         </a>
                     </nav>
-                    <div className='icons'>
-                        <span className='icon'>
+                    <div className={styles.icons}>
+                        <span className={styles.icon}>
                             <i className='fas fa-search' />
                         </span>
-                        <span className='icon'>
+                        <span className={styles.icon}>
                             <i className='fas fa-bell' />
                         </span>
-                        <span className='icon'>
+                        <span className={styles.icon}>
                             <i className='fas fa-user' />
                         </span>
                     </div>
                 </div>
             </header>
         </div>
-    )
+    );
 }
-export default Header
+
+export default Header;
--- src/pages/user/my-account/MyAccount.jsx
+++ src/pages/user/my-account/MyAccount.jsx
@@ -1,76 +1,76 @@
-import React from 'react'
-import styles from './Myaccount.module.css'
+import React from 'react';
+import './Myaccount.css';
 
 const MyAccount = () => {
   return (
-    <div className={styles.mainContainer}>
+    <div className="my-account__main-container">
         <div id="menu-placeholder" /> {/* Chỗ này sẽ được thay thế bởi menu */}
-        <div className={styles.profileContainer}>
-            <div className={styles.profileHeading}>
-            <div className={styles.profileTitle}>My Profile</div>
-            <div className={styles.profileDescription}>Manage and protect your account</div>
+        <div className="my-account__profile-container">
+            <div className="my-account__profile-heading">
+                <div className="my-account__profile-title">My Profile</div>
+                <div className="my-account__profile-description">Manage and protect your account</div>
             </div>
-            <hr className={styles.divider} />
-            <div className={styles.profileDetails}>
-            <div className={styles.detailRow}>
-                <div className={styles.detailLabel}>Name:</div>
-                <div className={styles.detailValue}>
-                <input type="text" defaultValue="Nguyễn Cao Danh" />
+            <hr className="my-account__divider" />
+            <div className="my-account__profile-details">
+                <div className="my-account__detail-row">
+                    <div className="my-account__detail-label">Name:</div>
+                    <div className="my-account__detail-value">
+                        <input type="text" defaultValue="Nguyễn Cao Danh" />
+                    </div>
+                </div>
+                <div className="my-account__detail-row">
+                    <div className="my-account__detail-label">Email:</div>
+                    <div className="my-account__detail-value">danh******@gmail.com</div>
+                    <button className="my-account__edit-button">Change</button>
+                </div>
+                <div className="my-account__detail-row">
+                    <div className="my-account__detail-label">Phone:</div>
+                    <div className="my-account__detail-value">********35</div>
+                    <button className="my-account__edit-button">Change</button>
+                </div>
+                <div className="my-account__detail-row">
+                    <div className="my-account__detail-label">Gender:</div>
+                    <div className="my-account__detail-value">
+                        <input type="radio" id="male" name="gender" defaultValue="male" />
+                        <label htmlFor="male">Male</label>
+                        <input type="radio" id="female" name="gender" defaultValue="female" />
+                        <label htmlFor="female">Female</label>
+                    </div>
+                </div>
+                <div className="my-account__detail-row">
+                    <div className="my-account__detail-label">Birthday:</div>
+                    <div className="my-account__birthday-inputs">
+                        <select className="my-account__birthday-input" id="day">
+                            <option value="" disabled="" selected="">2</option>
+                            {/* Thêm các option từ 1 đến 31 */}
+                        </select>
+                        <select className="my-account__birthday-input" id="month">
+                            <option value="" disabled="" selected="">12</option>
+                            {/* Thêm các option từ 1 đến 12 */}
+                        </select>
+                        <select className="my-account__birthday-input" id="year">
+                            <option value="" disabled="" selected="">2002</option>
+                            {/* Thêm các option từ 1900 đến năm hiện tại */}
+                        </select>
+                    </div>
+                </div>
+                <div className="my-account__action-buttons">
+                    <div className="my-account__button-container">
+                        <button className="my-account__save-button">Save</button>
+                    </div>
                 </div>
             </div>
-            <div className={styles.detailRow}>
-                <div className={styles.detailLabel}>Email:</div>
-                <div className={styles.detailValue}>danh******@gmail.com</div>
-                <button className={styles.editButton}>Change</button>
-            </div>
-            <div className={styles.detailRow}>
-                <div className={styles.detailLabel}>Phone:</div>
-                <div className={styles.detailValue}>********35</div>
-                <button className={styles.editButton}>Change</button>
-            </div>
-            <div className={styles.detailRow}>
-                <div className={styles.detailLabel}>Gender:</div>
-                <div className={styles.detailValue}>
-                <input type="radio" id="male" name="gender" defaultValue="male" />
-                <label htmlFor="male">Male</label>
-                <input type="radio" id="female" name="gender" defaultValue="female" />
-                <label htmlFor="female">Female</label>
+            <div className="my-account__profile-image">
+                <input type="file" id="file-input" className="my-account__file-input"/>
+                <label htmlFor="file-input" className="my-account__file-input-label">Select Image</label>
+                <div className="my-account__file-info">
+                    <p>File size: maximum 1 MB</p>
+                    <p>File extension: JPEG, PNG</p>
                 </div>
-            </div>
-            <div className={styles.detailRow}>
-                <div className={styles.detailLabel}>Birthday:</div>
-                <div className={styles.birthdayInputs}>
-                <select className={styles.birthdayInput} id="day">
-                    <option value="" disabled="" selected="">2</option>
-                    {/* Thêm các option từ 1 đến 31 */}
-                </select>
-                <select className={styles.birthdayInput} id="month">
-                    <option value="" disabled="" selected="">12</option>
-                    {/* Thêm các option từ 1 đến 12 */}
-                </select>
-                <select className={styles.birthdayInput} id="year">
-                    <option value="" disabled="" selected="">2002</option>
-                    {/* Thêm các option từ 1900 đến năm hiện tại */}
-                </select>
-                </div>
-            </div>
-            <div className={styles.actionButtons}>
-                <div className={styles.buttonContainer}>
-                <button className={styles.saveButton}>Save</button>
-                </div>
-            </div>
-            </div>
-            <div className={styles.profileImage}>
-            <input type="file" id="file-input" className={styles.fileInput}/>
-            <label htmlFor="file-input" className={styles.fileInputLabel}>Select Image</label>
-            <div className={styles.fileInfo}>
-                <p>File size: maximum 1 MB</p>
-                <p>File extension: JPEG, PNG</p>
-            </div>
             </div>
         </div>
     </div>
-  )
+  );
 }
 
-export default MyAccount
+export default MyAccount;
